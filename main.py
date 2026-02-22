1c82e413c9ee25e9d254d50630d8e39b0fa1a5b4
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-22 09:52:23 +0200

    Add first routes

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from config-service!")
+from fastapi import FastAPI
+from sqlmodel import create_engine, Session, SQLModel, select
+
+from models.config_item import KeyValue
+
+app = FastAPI()
+engine = create_engine("sqlite:///database.db")
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
+
+
+# @app.get("/key_value/{project_id}/{key}")
+# async def get_key_value(project_id: str, key: str):
+#     with Session(engine) as session:
+#         item = session.exec(KeyValue).filter(KeyValue.key == key).first()
+
+#         print(type(item))
+#         if item:
+#             return {"key": item.key, "value": item.value}
+#         else:
+#             return {"error": "Key not found"}, 404
+
+
+@app.get("/key_value/{project_id}")
+async def get_all_key_values(project_id: str):
+    with Session(engine) as session:
+        statement = select(KeyValue).where(KeyValue.project_id == project_id)
+        results = session.exec(statement)
+
+        return [{"key": item.key, "value": item.value} for item in results]
+
+
+@app.post("/key_value")
+async def create_key_value(item: KeyValue):
+    with Session(engine) as session:
+        session.add(item)
+        session.commit()
+        session.refresh(item)
+        return {"key": item.key, "value": item.value}
