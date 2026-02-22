37772700e3c259261a3cc325b23cbff837afc342
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-22 10:05:59 +0200

    Add more endpoints

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from fastapi import FastAPI
-from sqlmodel import create_engine, Session, SQLModel, select
+from sqlmodel import create_engine, Session, select
 
-from models.config_item import KeyValue
+from models.config_item import KeyValue, Project
 
 app = FastAPI()
 engine = create_engine("sqlite:///database.db")
 
 
-@app.get("/ping")
+@app.get("/health")
 async def ping():
-    return {"message": "pong"}
+    return {"message": "healthy"}
 
 
-# @app.get("/key_value/{project_id}/{key}")
-# async def get_key_value(project_id: str, key: str):
-#     with Session(engine) as session:
-#         item = session.exec(KeyValue).filter(KeyValue.key == key).first()
+@app.get("/project")
+async def get_all_projects():
+    with Session(engine) as session:
+        statement = select(Project)
+        results = session.exec(statement)
 
-#         print(type(item))
-#         if item:
-#             return {"key": item.key, "value": item.value}
-#         else:
-#             return {"error": "Key not found"}, 404
+        return results.all()
 
 
 @app.get("/key_value/{project_id}")
@@ -33,10 +30,24 @@ async def get_all_key_values(project_id: str):
         return [{"key": item.key, "value": item.value} for item in results]
 
 
-@app.post("/key_value")
-async def create_key_value(item: KeyValue):
+@app.get("/key_value/{project_id}/{key}")
+async def get_key_value(project_id: str, key: str):
     with Session(engine) as session:
+        statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
+        result = session.exec(statement).first()
+
+        if result:
+            return {"key": result.key, "value": result.value}
+        else:
+            return {"error": "Key not found"}
+
+
+@app.post("/key_value/{project_id}/{key}/{value}")
+async def create_key_value(project_id: str, key: str, value: str):
+    with Session(engine) as session:
+        item = KeyValue(project_id=project_id, key=key, value=value)
+
         session.add(item)
         session.commit()
         session.refresh(item)
-        return {"key": item.key, "value": item.value}
+        return {"status": "OK"}
