81eceaf6950e761c41e10d0c134f993738acef66
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-22 18:13:06 +0200

    Add users models

---
+++ src/cove/models/users.py
@@ -0,0 +1,15 @@
+import uuid
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    username: str
+    password_hash: str
+
+
+class APIKey(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    user_id: str = Field(foreign_key="user.id")
+    key: str
+    access_for_project_id: str = Field(foreign_key="project.id")
