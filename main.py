54852196dd37e86570dab7c709eb807d7a061346
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-22 14:26:03 +0200

    Add DB as Depends

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
 from sqlmodel import create_engine, Session, select
 
 from models.config_item import KeyValue, Project
@@ -7,47 +8,48 @@ app = FastAPI()
 engine = create_engine("sqlite:///database.db")
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 @app.get("/health")
 async def ping():
     return {"message": "healthy"}
 
 
 @app.get("/project")
-async def get_all_projects():
-    with Session(engine) as session:
-        statement = select(Project)
-        results = session.exec(statement)
+async def get_all_projects(session: Annotated[Session, Depends(get_session)]):
+    statement = select(Project)
+    results = session.exec(statement)
 
-        return results.all()
+    return results.all()
 
 
 @app.get("/key_value/{project_id}")
-async def get_all_key_values(project_id: str):
-    with Session(engine) as session:
-        statement = select(KeyValue).where(KeyValue.project_id == project_id)
-        results = session.exec(statement)
+async def get_all_key_values(project_id: str, session: Annotated[Session, Depends(get_session)]):
+    statement = select(KeyValue).where(KeyValue.project_id == project_id)
+    results = session.exec(statement)
 
-        return [{"key": item.key, "value": item.value} for item in results]
+    return [{"key": item.key, "value": item.value} for item in results]
 
 
 @app.get("/key_value/{project_id}/{key}")
-async def get_key_value(project_id: str, key: str):
-    with Session(engine) as session:
-        statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
-        result = session.exec(statement).first()
+async def get_key_value(project_id: str, key: str, session: Annotated[Session, Depends(get_session)]):
+    statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
+    result = session.exec(statement).first()
 
-        if result:
-            return {"key": result.key, "value": result.value}
-        else:
-            return {"error": "Key not found"}
+    if result:
+        return {"key": result.key, "value": result.value}
+    else:
+        return {"error": "Key not found"}
 
 
 @app.post("/key_value/{project_id}/{key}/{value}")
-async def create_key_value(project_id: str, key: str, value: str):
-    with Session(engine) as session:
-        item = KeyValue(project_id=project_id, key=key, value=value)
+async def create_key_value(project_id: str, key: str, value: str, session: Annotated[Session, Depends(get_session)]):
+    item = KeyValue(project_id=project_id, key=key, value=value)
 
-        session.add(item)
-        session.commit()
-        session.refresh(item)
-        return {"status": "OK"}
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+    return {"status": "OK"}
