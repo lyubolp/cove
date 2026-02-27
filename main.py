18d3ff624a95d7bd846141406f8cd43cff3e1ed5
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-27 23:08:14 +0200

    Add users routes to main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from cove.routes import key_value, projects
+from cove.routes import key_value, projects, users
 
 app = FastAPI()
 
 
 app.include_router(key_value.router)
 app.include_router(projects.router)
+app.include_router(users.router)
 
 
 @app.get("/health")
