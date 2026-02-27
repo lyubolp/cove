d715ddaf3a7baf9bfda688748c03372d31dc7898
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-02-27 21:42:43 +0200

    Enhance project route functions with type hints and improve visibility toggle logic

--- src/cove/routes/projects.py
+++ src/cove/routes/projects.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Sequence
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/project")
 
 
 @router.get("/")
-async def get_all_projects(session: Annotated[Session, Depends(get_session)]):
+async def get_all_projects(session: Annotated[Session, Depends(get_session)]) -> Sequence[Project]:
     statement = select(Project)
     results = session.exec(statement)
 
@@ -18,7 +18,7 @@ async def get_all_projects(session: Annotated[Session, Depends(get_session)]):
 
 
 @router.post("/{name}")
-async def create_project(name: str, session: Annotated[Session, Depends(get_session)]):
+async def create_project(name: str, session: Annotated[Session, Depends(get_session)]) -> dict[str, str]:
     project = Project(name=name, is_public=False)
 
     session.add(project)
@@ -27,18 +27,39 @@ async def create_project(name: str, session: Annotated[Session, Depends(get_sess
     return {"status": "OK", "project_id": project.id}
 
 
-@router.patch("/{project_id}/public")
+@router.patch("/{project_id}")
 async def toggle_project_visibility(
-    project_id: str, is_public: bool, session: Annotated[Session, Depends(get_session)]
-):
+    project_id: str,
+    session: Annotated[Session, Depends(get_session)],
+    name: str | None = None,
+    is_public: bool | None = None,
+) -> dict[str, str]:
     statement = select(Project).where(Project.id == project_id)
     project = session.exec(statement).first()
 
     if project:
-        project.is_public = is_public
+        if is_public is not None:
+            project.is_public = is_public
+
+        if name is not None:
+            project.name = name
+
         session.add(project)
         session.commit()
         session.refresh(project)
+        return {"status": "OK"}
+    else:
+        return {"error": "Project not found"}
+
+
+@router.delete("/{project_id}")
+async def delete_project(project_id: str, session: Annotated[Session, Depends(get_session)]) -> dict[str, str]:
+    statement = select(Project).where(Project.id == project_id)
+    project = session.exec(statement).first()
+
+    if project:
+        session.delete(project)
+        session.commit()
         return {"status": "OK"}
     else:
         return {"error": "Project not found"}
