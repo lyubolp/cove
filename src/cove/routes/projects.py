33003968a8a24099be0e36c91e1951cdc29ed2f4
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-03 16:59:54 +0200

    Add route for extending access to project

--- src/cove/routes/projects.py
+++ src/cove/routes/projects.py
@@ -32,11 +32,15 @@ async def get_all_projects(
     private_projects_with_access = []
 
     if current_user is not None:
-        private_projects_with_access = [
-            project
-            for project in projects
-            if not project.is_public and await does_user_have_access_to_project(session, current_user, project.id)
-        ]
+        project_under_user_statement = select(ProjectUserLink).where(ProjectUserLink.user_id == current_user.id)
+        user_links = session.exec(project_under_user_statement).all()
+
+        for user_link in user_links:
+            statement = select(Project).where(Project.id == user_link.project_id)
+            project = session.exec(statement).first()
+
+            if project and not project.is_public:
+                private_projects_with_access.append(project)
 
     return public_projects + private_projects_with_access
 
@@ -99,3 +103,13 @@ async def delete_project(project_id: str, session: Annotated[Session, Depends(ge
         return {"status": "OK"}
     else:
         return {"error": "Project not found"}
+
+
+@router.post("/{project_id}/access/{user_id}", dependencies=[Depends(get_current_user_with_project_access)])
+async def add_user_to_project(
+    project_id: str, user_id: str, session: Annotated[Session, Depends(get_session)]
+) -> dict[str, str]:
+    link = ProjectUserLink(project_id=project_id, user_id=user_id)
+    session.add(link)
+    session.commit()
+    return {"status": "OK"}
