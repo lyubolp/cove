860fcb2dd14df14181c59d486ffa4b9125ce963d
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-07 00:17:29 +0200

    Items in private projects are accessible with api keys

--- src/cove/routes/key_value.py
+++ src/cove/routes/key_value.py
@@ -8,11 +8,13 @@ from cove.models.config_item import KeyValue
 from ..dependencies import get_session
 from ..models.projects import Project
 from ..models.users import User
+from ..services.auth.api_keys import (
+    api_key_header,
+    does_api_key_grant_access_to_project,
+)
 from ..services.auth.oauth2 import (
     does_user_have_access_to_project,
-    get_current_user,
     get_current_user_non_fatal,
-    get_current_user_with_project_access,
 )
 
 router = APIRouter(prefix="/key_value")
@@ -23,6 +25,7 @@ async def get_all_key_values(
     project_id: str,
     session: Annotated[Session, Depends(get_session)],
     current_user: Annotated[User | None, Depends(get_current_user_non_fatal)],
+    api_key: Annotated[str | None, Depends(api_key_header)],
 ):
     project_statement = select(Project).where(Project.id == project_id)
     project = session.exec(project_statement).first()
@@ -30,12 +33,18 @@ async def get_all_key_values(
     if project is None:
         raise HTTPException(status_code=404, detail="Project not found")
 
-    statement = select(KeyValue).where(KeyValue.project_id == project_id)
-
     if not project.is_public:
-        if current_user is None or not await does_user_have_access_to_project(session, current_user, project_id):
+        if current_user is None and api_key is None:
             raise HTTPException(status_code=403, detail="User does not have access to this project")
+        else:
+            if current_user is not None and not await does_user_have_access_to_project(
+                session, current_user, project_id
+            ):
+                raise HTTPException(status_code=403, detail="User does not have access to this project")
+            elif api_key is not None and not does_api_key_grant_access_to_project(session, api_key, project_id):
+                raise HTTPException(status_code=403, detail="User does not have access to this project")
 
+    statement = select(KeyValue).where(KeyValue.project_id == project_id)
     items = session.exec(statement)
 
     return [{"key": item.key, "value": item.value} for item in items]
@@ -47,7 +56,26 @@ async def get_key_value(
     key: str,
     session: Annotated[Session, Depends(get_session)],
     current_user: Annotated[User | None, Depends(get_current_user_non_fatal)],
+    api_key: Annotated[str | None, Depends(api_key_header)],
 ):
+    project_statement = select(Project).where(Project.id == project_id)
+    project = session.exec(project_statement).first()
+
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    # TODO - This seems to be repeated
+    if not project.is_public:
+        if current_user is None and api_key is None:
+            raise HTTPException(status_code=403, detail="User does not have access to this project")
+        else:
+            if current_user is not None and not await does_user_have_access_to_project(
+                session, current_user, project_id
+            ):
+                raise HTTPException(status_code=403, detail="User does not have access to this project")
+            elif api_key is not None and not does_api_key_grant_access_to_project(session, api_key, project_id):
+                raise HTTPException(status_code=403, detail="User does not have access to this project")
+
     statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
     result = session.exec(statement).first()
 
@@ -55,7 +83,7 @@ async def get_key_value(
         return {"error": "Key not found"}
 
     return {"key": result.key, "value": result.value}
-    
+
 
 @router.post("/{project_id}/{key}/{value}")
 async def create_key_value(
@@ -63,9 +91,16 @@ async def create_key_value(
     key: str,
     value: str,
     session: Annotated[Session, Depends(get_session)],
-    current_user: Annotated[User, Depends(get_current_user)],
+    current_user: Annotated[User | None, Depends(get_current_user_non_fatal)],
+    api_key: Annotated[str | None, Depends(api_key_header)],
 ):
-    if await does_user_have_access_to_project(session, current_user, project_id) is False:
+    if current_user is None and api_key is None:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if current_user is not None and await does_user_have_access_to_project(session, current_user, project_id) is False:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if api_key is not None and does_api_key_grant_access_to_project(session, api_key, project_id) is False:
         raise HTTPException(status_code=403, detail="User does not have access to this project")
 
     item = KeyValue(project_id=project_id, key=key, value=value)
@@ -77,14 +112,25 @@ async def create_key_value(
     return {"status": "OK"}
 
 
-@router.patch("/{project_id}/{key}", dependencies=[Depends(get_current_user_with_project_access)])
+@router.patch("/{project_id}/{key}")
 async def update_key_value(
     project_id: str,
     key: str,
     session: Annotated[Session, Depends(get_session)],
+    current_user: Annotated[User | None, Depends(get_current_user_non_fatal)],
+    api_key: Annotated[str | None, Depends(api_key_header)],
     value: str | None = None,
     is_public: bool | None = None,
 ):
+    if current_user is None and api_key is None:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if current_user is not None and await does_user_have_access_to_project(session, current_user, project_id) is False:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if api_key is not None and does_api_key_grant_access_to_project(session, api_key, project_id) is False:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
     statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
     item = session.exec(statement).first()
 
@@ -103,8 +149,24 @@ async def update_key_value(
         return {"error": "Key not found"}
 
 
-@router.delete("/{project_id}/{key}", dependencies=[Depends(get_current_user_with_project_access)])
-async def delete_key_value(project_id: str, key: str, session: Annotated[Session, Depends(get_session)]):
+@router.delete("/{project_id}/{key}")
+async def delete_key_value(
+    project_id: str,
+    key: str,
+    session: Annotated[Session, Depends(get_session)],
+    current_user: Annotated[User | None, Depends(get_current_user_non_fatal)],
+    api_key: Annotated[str | None, Depends(api_key_header)],
+):
+
+    if current_user is None and api_key is None:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if current_user is not None and await does_user_have_access_to_project(session, current_user, project_id) is False:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
+    if api_key is not None and does_api_key_grant_access_to_project(session, api_key, project_id) is False:
+        raise HTTPException(status_code=403, detail="User does not have access to this project")
+
     statement = select(KeyValue).where(KeyValue.project_id == project_id, KeyValue.key == key)
     item = session.exec(statement).first()
 
