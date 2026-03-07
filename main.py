b5016e216d5fb5fb11b7245edbef4ee61d98ae34
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-03-07 10:08:14 +0200

    Pin versions

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 Registers all route routers and exposes a /health endpoint.
 """
 
+from importlib.metadata import version
+
 from fastapi import FastAPI
 
 from cove.routes import api_keys, key_value, projects, users
 
-app = FastAPI()
+app = FastAPI(version=version("cove"))
 
 
 app.include_router(api_keys.router)
